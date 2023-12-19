95a32500ed81326b3842c3dae2f6da9afc5653b4
Ataur Rahman <rahman.sharif39n@gmail.com>
2023-12-19 10:13:55 +0600

    signup page functionality added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { store } from '@/Redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Provider store={store}>
+    <Toaster />
     <Component {...pageProps} />
   </Provider>
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,30 +1,91 @@
-import Link from "next/link"
-import { Button, FloatingLabel, Form } from "react-bootstrap"
+import { useSignUpMutation } from "@/Redux/features/Auth/auth.api";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Button, Col, FloatingLabel, Form, Row } from "react-bootstrap";
+import toast from "react-hot-toast";
 
 const SignUp = () => {
+    const [info, setInfo] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+    })
+    const [signup, options] = useSignUpMutation()
+
+    const router = useRouter()
+
+    useEffect(() => {
+        options.isLoading && toast.loading('Please wait...', {
+            id: 'signup',
+        })
+        if (options.isSuccess) {
+            toast.success(options.data.message, {
+                id: 'signup',
+            })
+            router.replace('/signin')
+        }
+
+        options.isError && toast.error((options.error as any).data.message, {
+            id: 'signup',
+        })
+    }, [options])
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        try {
+            signup({
+                name: {
+                    firstName: info.firstName,
+                    lastName: info.lastName
+                },
+                email: info.email,
+                password: info.password
+            })
+
+
+        } catch (error) {
+
+        }
+    }
+
     return (
         <div className="container d-flex justify-content-center align-items-center" style={{
             height: '100vh'
         }}>
             <div className="w-50">
                 <h1 className="mb-5 text-center">Sign up</h1>
-                <div >
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Full Name"
-                        className="mb-3"
-                    >
-                        <Form.Control type="text" placeholder="Alex Devin" />
-                    </FloatingLabel>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <Row>
+                        <Col>
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                label="First Name"
+                                className="mb-3"
+                            >
+                                <Form.Control onChange={(e) => setInfo({ ...info, [e.target.name]: e.target.value })} type="text" placeholder="Alex" name="firstName" required />
+                            </FloatingLabel>
+                        </Col>
+                        <Col>
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                label="Last Name"
+                                className="mb-3"
+                            >
+                                <Form.Control onChange={(e) => setInfo({ ...info, [e.target.name]: e.target.value })} type="text" placeholder="Devin" name="lastName" required />
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
                     <FloatingLabel
                         controlId="floatingInput"
                         label="Email address"
                         className="mb-3"
                     >
-                        <Form.Control type="email" placeholder="name@example.com" />
+                        <Form.Control onChange={(e) => setInfo({ ...info, [e.target.name]: e.target.value })} type="email" placeholder="name@example.com" name="email" required />
                     </FloatingLabel>
                     <FloatingLabel controlId="floatingPassword" label="Password">
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control onChange={(e) => setInfo({ ...info, [e.target.name]: e.target.value })} type="password" placeholder="Password" name="password" required />
                     </FloatingLabel>
                     <div className="m-3">
                         <Link href={'/signin'} className="">
@@ -32,9 +93,9 @@ const SignUp = () => {
                         </Link>
                     </div>
                     <div className="p-3" >
-                        <Button size="lg" variant="outline-dark">Sign up</Button>
+                        <Button type="submit" size="lg" variant="outline-dark">Sign up</Button>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     )
